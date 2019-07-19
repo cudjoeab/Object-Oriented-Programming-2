d882fc23b799d4c931fc58f618705ae65974f533
cudjoeab <cudjoeab@gmail.com>
2019-07-18 21:16:51 -0400

    folks can add, borrow and give back books

--- book_lending.py
+++ book_lending.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import random 
 
 class Book:
 
@@ -11,19 +12,21 @@ class Book:
     self.ISBN = ISBN
 
   @classmethod
-  def create(cls):
-      new_book = Book()
+  def create(cls, title, author, ISBN):
+      new_book = Book(title, author, ISBN)
       cls.on_shelf.append(new_book)
 
   @classmethod
-  def check_out_time(cls):
-      now = datetime.now()
-      now.timestamp()
-      return now 
+  def browse(cls): 
+      browse = random.choice(cls.on_shelf)
+      return browse 
 
   @classmethod
-  def due_date(cls): 
-      one_week = 
+  def current_due_date(cls): 
+    now = datetime.now()
+    two_weeks = 60 * 60 * 24 * 14 # two weeks expressed in seconds  
+    future_timestamp = now.timestamp() + two_weeks
+    return datetime.fromtimestamp(future_timestamp)
       
 
 
@@ -34,7 +37,7 @@ class Book:
 
   def borrow(self):
     book = Book()
-    if on_shelf[book] == True: 
+    if self.on_shelf[book] == True: 
      on_loan.append(book)
     return f'{self.title} by {self.author} is now on loan.'
 
@@ -48,7 +51,25 @@ class Book:
 
 
 book = Book('I know why the caged bird sings', 'Maya Angelou', 12345678910)
-print(book)
+sister_outsider = Book.create("Sister Outsider", "Audre Lorde", "9781515905431")
+aint_i = Book.create("Ain't I a Woman?", "Bell Hooks", "9780896081307")
+if_they_come = Book.create("If They Come in the Morning", "Angela Y. Davis", "0893880221")
+print(Book.browse().title) # "Sister Outsider" (this value may be different for you)
+print(Book.browse().title) # "Ain't I a Woman?" (this value may be different for you)
+print(len(Book.on_shelf)) # 3
+print(len(Book.on_loan)) # 0
+print(sister_outsider.lent_out()) # False
+print(sister_outsider.borrow()) # True
+print(len(Book.on_shelf)) # 2
+print(len(Book.on_loan)) # 1
+print(sister_outsider.lent_out()) # True
+print(sister_outsider.borrow()) # False
+print(sister_outsider.due_date) # 2017-02-25 20:52:20 -0500 (this value will be different for you)
+print(len(Book.overdue())) # 0
+print(sister_outsider.return_to_library()) # True
+print(sister_outsider.lent_out()) # False
+print(len(Book.on_shelf)) # 2
+print(len(Book.on_loan)) # 0
  
 
 
